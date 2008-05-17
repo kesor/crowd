b48df24df28c4321727bc5f2977c41bf6ddc68c5
Evgeny Zislis <evgeny.zislis@gmail.com>
2008-05-17 23:38:55 +0300

    add an "interface"/skeleton for SSO implementation

---
+++ lib/crowd/http/request.rb
@@ -0,0 +1,6 @@
+##
+# An HTTP request. This class holds data from the client
+# It allows the HTTP-protocol specified header information to be
+# accessed from the SSO class.
+class Crowd::HTTP::Request < Net::HTTPRequest
+end
---
+++ lib/crowd/http/response.rb
@@ -0,0 +1,5 @@
+##
+# An HTTP response. This class allows to manipulate HTTP-protocol
+# specified header information and return data to its client.
+class Crowd::HTTP::Response < Net::HTTPResponse
+end
