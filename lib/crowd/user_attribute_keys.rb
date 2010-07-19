d7a3899ad404a6b18a257bc63b4c61c74a1e762b
Stefan Wille <post@stefanwille.com>
2010-07-19 22:45:33 +0800

    Add user_attribute_keys

---
+++ lib/crowd/user_attribute_keys.rb
@@ -0,0 +1,20 @@
+class Crowd
+
+  #
+  # Keys for user attributes
+  #
+  module UserAttributeKeys
+    USERNAME = "username"
+    FIRSTNAME = "givenName"
+    LASTNAME = "sn"
+    DISPLAYNAME = "displayName"
+    EMAIL = "mail"
+    ICON_LOCATION = "iconLocation"
+    PASSWORD_LASTCHANGED = "passwordLastChanged"
+    LAST_AUTHENTICATED = "lastAuthenticated"
+    INVALID_PASSWORD_ATTEMPTS = "invalidPasswordAttempts"
+    REQUIRES_PASSWORD_CHANGE = "requiresPasswordChange"
+    ACTIVE = "active"
+  end
+
+end
